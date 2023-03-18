9539bd6e2f45924e771482bc0dc744c70784d0b6
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-03-18 16:52:10 -0700

    footer added

--- josegomezdev/src/App.jsx
+++ josegomezdev/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./scenes/Navbar";
 import DotGroup from './scenes/DotGroup'
@@ -6,6 +7,8 @@ import Landing from "./scenes/Landing";
 import LineGradient from './components/LineGradient'
 import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
 
 function App() {
 
@@ -15,12 +18,15 @@ function App() {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY === 0) setIsTopOfPage(true);
-      if (window.scrollY !== 0) setIsTopOfPage(false)
-    }
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [])
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+        window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app bg-deep-blue app">
@@ -40,12 +46,30 @@ function App() {
 
       </div>
       <LineGradient />
+
+
       <div className="w-5/6 mx-auto md:h-full">
         <MySkills />
       </div>
+
+            <LineGradient />
+
       <div className="w-5/6 mx-auto md:h-full">
         <Projects/>
       </div>
+
+
+  <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("contact")}
+        >
+          <Contact />
+        </motion.div>
+      </div>
+      <Footer />
     </div>
 
   );
